cae73092e02cc1fc1ebeaa29698c0773bcd7cc85
אבישי אליחי <avishayelihay@gmail.com>
2023-02-23 00:10:09 +0200

    more database work

--- DataBase/datbase.js
+++ DataBase/datbase.js
@@ -1,3 +1,6 @@
+import { Mession } from "./mession";
+import { SubMession } from "./submession"
+
 
 export class Database {
 
@@ -8,44 +11,94 @@ export class Database {
         localStorage.setItem(key,JSON.stringify(value))
     }
 
-    static search(userId){
+    static getUser(userId){
         users = load('Users')
 
-        return users.
+        return users[userId.toString()]
     }
 
-    static get(userId){
+    static getUser(username,password){
+        users = load('Users')
 
+        for (const key in users){
+            if (users[key].username === username && users[key].password === password){
+                return users[key]
+            }
+        }
+        return undefined
+    }
+
+    static getMessions(userId){
+        user = this.getUser(userId)
+        messions  = load('Messions')
+        final_list = []
+        for (const mession of  messions){
+            if (mession.userId===userId){
+                final_list.push(mession)
+            }
+        }
+
+        return final_list
     }
 
     static addUser(user){
         users = load('Users')
 
-        user.id = users.length
-        users.push(user)
+        users[Object.keys(users).length.toString()] = JSON.stringify(user)
 
         save('Users', users)
     }
 
     static addMession(userId,text){
+        messions = load('Messions')
+        messions.push(JSON.stringify(Mession(text,userId,messions.length)))
 
+        save('Messions', messions)
     }
 
     static addSubMession(messionId,text){
+        submessions = load('SubMessions')
+        submessions.push(JSON.stringify(SubMession(text,messionId,submessions.length)))
 
+        save('SubMessions', submessions)
     }
 
-    static update(userId,mession){
+    static removeUser(userId){
+        users=load('Users')
 
+        delete users[userId.toString()]
+
+        save('Users',users)
     }
 
-    static remove(userId){
+    static removeMession(messionId){
+        messions=load('Messions')
 
+        messionIndex = messions.findIndex(function(mes){return mes.id==messionId})
+        
+        if (messionIndex){
+            messions.splice(messionIndex,1)
+        }
+        else{
+            console.log('Dont Exists')
+        }
+
+        save('Messions',messions)
     }
 
-    static remove(messionId){
+    static removeSubMession(submessionId){
+        submessions=load('SubMessions')
+        
+        messionIndex = submessions.findIndex(function(mes){return mes.id==messionId})
+        
+        if (messionIndex){
+            submessions.splice(messionIndex,1)
+        }
+        else{
+            console.log('Dont Exists')
+        }
 
+        save('SubMessions',submessions)
     }
-
 
 }
